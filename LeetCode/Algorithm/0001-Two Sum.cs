84d5b1292a6476baeb269707882b6822974b7e88
geniuscynic <350810375@qq.com>
2024-06-02 22:28:19 +0800

    0001 two sums

---
+++ LeetCode/Algorithm/0001-Two Sum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// 1. Two Sum
+        /// https://leetcode.com/problems/two-sum/description/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for(var i = 0; i < nums.Length;  i++)
+            {
+                if(dict.ContainsKey(target - nums[i])) 
+                {  
+                    return [i, dict[target - nums[i]]];
+                }
+
+                dict.TryAdd(nums[i], i);
+            }
+
+            return [0, 1];
+        }
+    }
+}
---
+++ LeetCode/Algorithm/0002-Add Two Numbers.cs
@@ -0,0 +1,54 @@
+﻿using net.entity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var result = new ListNode();
+            var header = result;
+            var digit = 0;
+            var res = 0;
+            while (l1 != null || l2 != null || digit == 1) {
+                if(l1 != null)
+                {
+                    res += l1.val;
+                }
+                if(l2 != null)
+                {
+                    res += l2.val;
+                }
+              
+                res += digit;
+                if(res >= 10)
+                {
+                    digit = 1;
+                    res -= 10;
+                }
+                else
+                {
+                    digit = 0;
+                }
+
+                result.next = new ListNode(res);
+                res = 0;
+                result = result.next;
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+
+
+            return header.next;
+        }
+    }
+}
